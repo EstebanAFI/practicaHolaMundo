a7dc299a9083e273a790e77d2d963335aed25342
Salva <salvadorhernandez2@kpmg.es>
2016-11-08 18:52:58 +0100

    PRIMERA VERSION CODIGO ALBERTO

---
+++ R/AFIHolaMundoAFJ.R
@@ -0,0 +1,23 @@
+#' Title Imprimir por pantalla Hola Mundo (Spanish) 
+#'
+#' @return Null
+#' @export 
+#'
+#' @examples HolaMundo()
+#' @export 
+HolaMundo<-function(){
+  print('Hola Mundo')
+}
+
+
+
+#' Title Imprimir por pantalla HelloWorld (English)
+#'
+#' @return Null
+#' 
+#'
+#' @examples HelloWorld() 
+#' @export 
+HelloWorld<-function(){
+  print('Hello World')
+}
